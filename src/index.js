342e00b70c359aab0a90dbc72849fb4c15c0114a
lohyaoxuan777 <jaydenloh57@gmail.com>
2025-04-19 18:51:20 +0800

    modified:   index.html 	modified:   src/index.js 	modified:   styles/style.css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-let button = document.querySelector("button");
+let button = document.querySelector(".button");
 let score = document.querySelector("p");
+
+let autoClickerBtn = document.querySelector(".auto-clicker");
+
 score.textContent = 0;
 score.style.display = "none";
 
 button.addEventListener("click", () => {
     score.style.display = "block";
+    button.textContent = "Click";
+    
     score.textContent = Number(score.textContent) + 1;
+    if (score.textContent == 10) {
+        autoClickerBtn.setAttribute("data-title", "Gets 1 point every 3 seconds");
+        autoClickerBtn.style.setProperty("cursor", "pointer");
+        autoClickerBtn.style.setProperty("background", "#000");
+
+        autoClickerBtn.style.setProperty("transition", "0.3s");         
+        autoClickerBtn.classList.add("button-hover-cmp", "button-active-cmp");
+
+        
+    };
+});
+
+autoClickerBtn.addEventListener("click", () => {
+    if (score.textContent >= 10) {
+        score.textContent = Number(score.textContent) - 10;
+        let interval = setInterval(() => {
+            score.textContent = Number(score.textContent) + 1;
+        }, 3000);
+
+        interval();
+    };
 });
