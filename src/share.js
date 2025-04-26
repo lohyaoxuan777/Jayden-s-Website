df8772c74d18bbea93abf61e71944c8a5fb5ca51
lohyaoxuan777 <jaydenloh57@gmail.com>
2025-04-26 13:57:07 +0800

    modified:   share.html 	modified:   src/share.js 	modified:   styles/share.css

--- src/share.js
+++ src/share.js
@@ -1,5 +1,10 @@
 let shareToWhatsappBtn = document.querySelector(".share-to-whatsapp");
+let shareToMailBtn = document.querySelector(".share-to-mail");
 
 shareToWhatsappBtn.addEventListener("click", () => {
-    window.open("https://api.whatsapp.com/send/?text=https://myboringwebsite.netlify.app/&app_absent=0")
+    window.open("https://api.whatsapp.com/send/?text=https://myboringwebsite.netlify.app/&app_absent=0");
+});
+
+shareToMailBtn.addEventListener("click", () => {
+    window.open("mailto:body=https://myboringwebsite.netlify.app/");
 });
